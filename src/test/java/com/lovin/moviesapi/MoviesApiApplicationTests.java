3874b3249b86c1e9889fb425c60e8745f7e290a7
jdlovin <josh.lovin@revature.net>
2021-09-13 21:34:17 -0400

    user fields with a test

--- src/test/java/com/lovin/moviesapi/MoviesApiApplicationTests.java
+++ src/test/java/com/lovin/moviesapi/MoviesApiApplicationTests.java
@@ -1,13 +1,27 @@
 package com.lovin.moviesapi;
 
+import com.lovin.moviesapi.entity.User;
+import com.lovin.moviesapi.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MoviesApiApplicationTests {
 
+	@Autowired
+	private UserRepository userRepository;
+
 	@Test
-	void contextLoads() {
+	public void saveUser(){
+		User user = User.builder()
+				.firstName("Bob")
+				.lastName("Saggot")
+				.password("1234")
+				.userName("bobs")
+				.userEmail("bob@gmail.com")
+				.build();
+		userRepository.save(user);
 	}
 
 }
