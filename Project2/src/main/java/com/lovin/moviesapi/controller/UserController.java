222f2a13498844751b7463d476269c709ff21ed9
Mpartx21 <miguel.p@revature.net>
2021-09-17 07:43:40 -0700

    update User Controllers

--- Project2/src/main/java/com/lovin/moviesapi/controller/UserController.java
+++ Project2/src/main/java/com/lovin/moviesapi/controller/UserController.java
@@ -39,8 +39,8 @@ public class UserController {
         return service.updateUser(userId, user);
     }
 
-    @GetMapping("/users/{email}/{password}")
-    public User getUserByCredentials(@PathVariable String email,@PathVariable String password){
+    @PostMapping("/users/login")
+    public User getUserByCredentials(@RequestBody String email,@RequestBody String password){
         return service.getUserByemailandPword(email,password);
     }
 
