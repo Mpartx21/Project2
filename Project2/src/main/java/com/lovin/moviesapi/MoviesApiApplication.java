6963ce2365f677f561e28a35adbff14d3d36363b
jdlovin <josh.lovin@revature.net>
2021-09-20 02:00:36 -0400

    fixed so angular didnt get CORS error

--- Project2/src/main/java/com/lovin/moviesapi/MoviesApiApplication.java
+++ Project2/src/main/java/com/lovin/moviesapi/MoviesApiApplication.java
@@ -3,7 +3,6 @@ package com.lovin.moviesapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
@@ -12,7 +11,6 @@ import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
-@EnableWebSecurity
 public class MoviesApiApplication {
 
 	public static void main(String[] args) {
