95fe7bad309850c30f07413fc5867457a48af985
jdlovin <josh.lovin@revature.net>
2021-09-19 22:16:40 -0400

    fixed movie search imports

--- Angular/movie-demo/src/app/components/movie-search/movie-search.component.ts
+++ Angular/movie-demo/src/app/components/movie-search/movie-search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from 'src/app/models/movie';
-import { DataService } from 'src/app/services/data.service';
+
+import { Movie } from 'src/app/models/movie/movie';
+
+import { DataService } from 'src/app/services/data/data.service';
 
 @Component({
   selector: 'app-movie-search',
