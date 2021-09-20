406ab1d7e61a3bf229e1d80da9e9e20b8b141980
jdlovin <josh.lovin@revature.net>
2021-09-19 22:23:27 -0400

    reimported imports movie details

--- Angular/movie-demo/src/app/components/movie-details/movie-details.component.ts
+++ Angular/movie-demo/src/app/components/movie-details/movie-details.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MovieDetail, Movie } from 'src/app/models/movie';
+
+
+
 import { ActivatedRoute } from '@angular/router';
-import { DataService } from 'src/app/services/data.service';
+
+import { DataService } from 'src/app/services/data/data.service';
 import { Observable } from 'rxjs';
 import { map, switchMap } from "rxjs/operators";
 
@@ -23,22 +26,22 @@ export class MovieDetailsComponent implements OnInit {
       map(queryParams => queryParams["movieId"]),
       switchMap(imdbId => this.data.getMovieDetails(imdbId)))
 
-   /* this.movieDetails = this.route.queryParams.pipe(
-      map(queryParams => queryParams["movieId"]),
-      switchMap(imdbId => this.data.getMovieDetails(imdbId)),
-      switchMap((movie: MovieDetail) =>
-        this.data.searchMovie(movie.Title).pipe(
-          map((similarMovies: Array<Movie>) =>
-            similarMovies.filter(
-              (similarMovie: Movie) => similarMovie.Title !== movie.Title
-            )
-          ),
-          map((similarMovies: Array<Movie>) => ({
-            ...movie,
-            similarMovies
-          }))
-        )
-      )
-    ); */
+    /* this.movieDetails = this.route.queryParams.pipe(
+       map(queryParams => queryParams["movieId"]),
+       switchMap(imdbId => this.data.getMovieDetails(imdbId)),
+       switchMap((movie: MovieDetail) =>
+         this.data.searchMovie(movie.Title).pipe(
+           map((similarMovies: Array<Movie>) =>
+             similarMovies.filter(
+               (similarMovie: Movie) => similarMovie.Title !== movie.Title
+             )
+           ),
+           map((similarMovies: Array<Movie>) => ({
+             ...movie,
+             similarMovies
+           }))
+         )
+       )
+     ); */
   }
 }
